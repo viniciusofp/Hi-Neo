00e38342eb390b849bf86ed09b3fd23697ba9560
viniciusofp <viniciusofp@gmail.com>
2018-10-16 14:13:28 -0300

    add playsinline attr to video bgs

--- themes/mytheme/header.php
+++ themes/mytheme/header.php
@@ -27,6 +27,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" crossorigin="anonymous"></script>
+
+
 
 	<?php wp_head(); ?>
 </head>
