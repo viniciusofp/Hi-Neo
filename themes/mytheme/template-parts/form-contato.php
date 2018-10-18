5b9e8c2bff3cec2e83e2f9a5b7605960943d2bcf
viniciusofp <viniciusofp@gmail.com>
2018-10-18 17:35:13 -0300

    wp integration complete?

--- themes/mytheme/template-parts/form-contato.php
+++ themes/mytheme/template-parts/form-contato.php
@@ -1,1 +1,8 @@
 <?php echo do_shortcode(get_field('formulario')) ?>
+<script type="text/javascript">
+		var wpcf7Elm = jQuery('#formcontato').closest('.wpcf7')[0];wpcf7Elm.addEventListener( 'wpcf7mailsent', function( event ) {__ss_noform.push(['submit', null, 'af07ccd4-76f5-46d1-b5d8-ca95c420fa1a']);}, false );
+    var __ss_noform = __ss_noform || [];
+    __ss_noform.push(['baseURI', 'https://app-3QNCT37ACG.marketingautomation.services/webforms/receivePostback/MzawMDEzMbe0AAA/']);
+    __ss_noform.push(['form', 'formcontato', 'af07ccd4-76f5-46d1-b5d8-ca95c420fa1a']);
+</script>
+<script type="text/javascript" src="https://koi-3QNCT37ACG.marketingautomation.services/client/noform.js?ver=1.24" ></script>
